44ae0017069aa43cc953171b2349c21f85ed4a77
connon523 <a3010205@yahoo.com.tw>
2022-01-01 23:00:21 +0800

    stander

---
+++ Assets/script/hit.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class hit : MonoBehaviour
+{
+    public KeyCode KeyToPress;
+
+    public int A;
+    public bool canBepress;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(A * Time.deltaTime, 0f);
+
+        if (Input.GetKeyDown(KeyToPress))
+        {
+            if (canBepress)
+            {                           
+                gameObject.SetActive(false);
+            }
+        }
+}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "good")
+        {
+            canBepress = true;
+        }
+        if(other.tag == "perfect")
+        {
+            canBepress = true;
+        }
+        if (other.tag == "miss")
+        {
+            canBepress = false;
+        
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "good")
+        {
+            canBepress = false;
+        }
+        if (other.tag == "perfect")
+        {
+            canBepress = false;
+        }
+    }
+}
---
+++ Assets/和風camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 和風camera : MonoBehaviour
+{
+    private float mSpeed = 6.0F;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+        transform.Translate(Vector3.right * Time.deltaTime * mSpeed);
+       
+       
+    }
+}
