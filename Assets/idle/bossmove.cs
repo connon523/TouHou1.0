1eb1af140a6db32417ebd9a5669e9da1cc0fb510
connon523 <a3010205@yahoo.com.tw>
2022-01-02 16:59:42 +0800

    combo

---
+++ Assets/idle/bossmove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossmove : MonoBehaviour
+{
+    float radian = 0;
+    float perRadian = 0.2f;
+    float radius = 3f;
+    Vector3 oldpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldpos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        radian += perRadian;
+
+        float dy = Mathf.Cos(radian) * radius;
+        transform.localPosition = oldpos + new Vector3(0, dy, 0);
+    }
+}
---
+++ Assets/script/scenes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scenes1timer : MonoBehaviour
+{
+    float time_f;
+    int timer_1;
+    public GameObject pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time_f = Time.time;
+        timer_1 = Mathf.FloorToInt(time_f);
+        Debug.Log(timer_1);
+        if (timer_1 == 60)
+        {
+            Show();
+        }
+    }
+    void Show()
+    {
+        pos.SetActive(true);
+    }
+    void Hide()
+    {
+        pos.SetActive(false);
+    }
+
+}
